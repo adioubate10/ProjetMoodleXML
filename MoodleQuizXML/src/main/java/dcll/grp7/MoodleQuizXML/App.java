a1fad50a6325efbc069f437d2cc1b1a25cfea918
dioppape <refane@live.fr>
2012-03-19 20:13:26 +0100

    rappel

--- MoodleQuizXML/src/main/java/dcll/grp7/MoodleQuizXML/App.java
+++ MoodleQuizXML/src/main/java/dcll/grp7/MoodleQuizXML/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! " );//dfgfg
+        System.out.println( "Hello World! " );//les gat au boulo maintenant
     }
 }
