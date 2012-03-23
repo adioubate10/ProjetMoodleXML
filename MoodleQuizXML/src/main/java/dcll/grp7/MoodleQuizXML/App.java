6ede2062caf925618828705c99820d823e1cd79b
dioppape <refane@live.fr>
2012-03-23 16:34:46 +0100

    test

--- MoodleQuizXML/src/main/java/dcll/grp7/MoodleQuizXML/App.java
+++ MoodleQuizXML/src/main/java/dcll/grp7/MoodleQuizXML/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! " );//gghhhhh
+        System.out.println( "Hello World! " );//new clone
     }
 }
