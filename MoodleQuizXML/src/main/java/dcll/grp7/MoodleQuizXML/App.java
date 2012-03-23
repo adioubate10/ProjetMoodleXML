a26c7062b1c7c9994821c9118521104493c13dab
Amadou <dioubateamadou@yahoo.fr>
2012-03-23 17:02:47 +0100

    modif

--- MoodleQuizXML/src/main/java/dcll/grp7/MoodleQuizXML/App.java
+++ MoodleQuizXML/src/main/java/dcll/grp7/MoodleQuizXML/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! " );//new clone
+        System.out.println( "Hello World! " );//clone
     }
 }
